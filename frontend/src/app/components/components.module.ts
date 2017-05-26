327caef243366241e89a8f71834dcbaa2ee19297
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-05-26 21:23:07 +0700

    Init VerticalNav component and compose raw template

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeftNavComponent } from './left-nav/left-nav.component';
+import { VerticalNavComponent } from './vertical-nav/vertical-nav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LeftNavComponent],
+  declarations: [
+    LeftNavComponent,
+    VerticalNavComponent
+  ],
   exports: [
-    LeftNavComponent
+    LeftNavComponent,
+    VerticalNavComponent
   ]
 })
 export class ComponentsModule { }
